289f5b5283a80dec3228d4351b955fe78ca686fb
jsapota <jsapota@wp.pl>
2017-10-15 23:15:46 +0200

    Gallery v 1.0 (#3)

---
+++ client/views/productGallery/productGallery.js
@@ -0,0 +1,54 @@
+Template.productGallery.helpers({
+   'categories': function () {
+       return [{"name": "hoodie"}, {"name": "tshirt"}];
+   },
+    'clothesList': function () {
+        return [
+            {
+                "name": "Blahoodie",
+                "creator": "sdf",
+                "price": 122,
+                "type": "sdf",
+                "size": "m",
+                "photo": "/images/hoodie1.jpg",
+                "color": "white",
+                "amount": 13,
+                "status": "available"
+            },
+            {
+                "name": "Blahoodie",
+                "creator": "sdf",
+                "price": 122,
+                "type": "sdf",
+                "size": "m",
+                "photo": "/images/hoodie1.jpg",
+                "color": "white",
+                "amount": 13,
+                "status": "available"
+            },
+            {
+                "name": "Blahoodie",
+                "creator": "sdf",
+                "price": 122,
+                "type": "sdf",
+                "size": "m",
+                "photo": "/images/hoodie1.jpg",
+                "color": "white",
+                "amount": 13,
+                "status": "available"
+            },           {
+                "name": "Blahoodie",
+                "creator": "sdf",
+                "price": 122,
+                "type": "sdf",
+                "size": "m",
+                "photo": "/images/hoodie1.jpg",
+                "color": "white",
+                "amount": 13,
+                "status": "available"
+            },
+
+        ];
+    },
+
+});
