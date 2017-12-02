02537e15ebb35cfe8af81a428fad65449b360355
Jakub Sapota <jsapota@wp.pl>
2017-12-02 22:40:46 +0100

    finished cart

--- client/views/cart/cart.js
+++ client/views/cart/cart.js
@@ -7,11 +7,19 @@ Template.cart.onCreated(function () {
   });
   this.totalPrice = new ReactiveVar(total);
   this.stage = new ReactiveVar(1);
+  this.orderDetails = new ReactiveVar();
+  this.error = new ReactiveVar("");
 });
 
 Template.cart.onRendered(function() {});
 
 Template.cart.helpers({
+  'isThereError': () => {
+    return "" !== Template.instance().error.get();
+  },
+  'error': () => {
+    return Template.instance().error.get();
+  },
   'stageOne': () => {
     return Template.instance().stage.get() === 1;
   },
@@ -39,6 +47,16 @@ Template.cart.helpers({
   },
   'productName': (id) => {
     return Clothes.findOne({_id: id}).name;
+  },
+  'orderDetails': () => {
+    let details = Template.instance().orderDetails.get();
+    return {
+      'name': details[0],
+      'surname': details[1],
+      'street': details[2],
+      'city': details[3],
+      'country': details[4]
+    }
   }
 });
 
@@ -50,9 +68,35 @@ Template.cart.events({
     Meteor.call('updateCart', event.target.value, -1, event.target.getAttribute('data-size'));
   },
   'click .btn-order': () => {
-    Template.instance().stage.set(Template.instance().stage.get() + 1);
+      Template.instance().stage.set(Template.instance().stage.get() + 1);
+      Template.instance().error.set("");
   },
   'click .btn-back': () => {
+    if(Template.instance().stage.get() === 3){
+      let inputValues = document.querySelectorAll('.recData');
+      let values = Template.instance().orderDetails.get();
+      let i = 0;
+      inputValues.forEach((field) => {
+        field.value = values[i];
+      })
+    };
+    Template.instance().error.set("");
     Template.instance().stage.set(Template.instance().stage.get() - 1);
   },
+  'click .btn-form': () => {
+    let inputs = document.querySelectorAll('.recData');
+    let values = [];
+    let error = "";
+    inputs.forEach((value) => {
+      values.push(value.value);
+      if(value.value.length === 0)
+        error = "Check if all fields are correct before you confirm."
+    });
+    if( error === ""){
+      Template.instance().orderDetails.set(values);
+      Template.instance().stage.set(Template.instance().stage.get() + 1);
+    } else {
+      Template.instance().error.set({'error': error});
+    }
+  }
 });
