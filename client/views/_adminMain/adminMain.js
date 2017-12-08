6859d017f5ae9a6fa6014484659fe41d87a5fc19
jsapota <jsapota@wp.pl>
2017-12-08 12:35:42 +0100

    J rpanel (#15)
    * added 2 divs: budget and deliveries. To be developed
    
    * added some add + buttons, without routing
    
    * added some add + buttons, without routing

--- client/views/_adminMain/adminMain.js
+++ client/views/_adminMain/adminMain.js
@@ -2,4 +2,18 @@ Template.adminMain.onCreated( function(){
 });
 
 Template.adminMain.helpers({
+    'shortDate': (date) => {
+        let temp = "" + date;
+        return temp.substring(0, 16);
+    },
+
+    'deliveryList': () => {
+        return [{name: "Zara Poland", size: "1000", details: "some details"}, {name: "BitterSweet", size: "200", details: "some details"}, {name: "Glamour", size: "350", details: "some details"}]
+    },
+
+    'shortDate': (date) => {
+        rand = _.random(0,3);
+        let temp = new Date(2018, rand, rand*2-4);
+        return temp.toDateString().substring(4, 16);
+    }
 });
